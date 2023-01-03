b68987b9fbf04c56bfe5bc740eb8730577622517
anchous <leonovivan171@gmail.com>
2023-01-03 21:15:28 +0300

    pygame start window

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import sys
 from tools import *
 
+
+clock = pygame.time.Clock()
+pygame.mixer.pre_init(44100, -16, 2, 512)
+pygame.init()
+
+WSIZE = WIDTH, HEIGHT = 600, 800
+screen = pygame.display.set_mode(WSIZE)
+FPS = 100
+
+
+def main_menu():
+    running = True
+    while running:
+        screen.fill((40, 40, 40))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        pygame.display.update()
+        clock.tick(FPS)
+
+
 if __name__ == '__main__':
-    pygame.init()
-    size = width, height = 800, 600
-    screen = pygame.display.set_mode(size)
-    # TODO: drawing on screen
-    pygame.display.flip()
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
-    pygame.quit()
+    main_menu()
