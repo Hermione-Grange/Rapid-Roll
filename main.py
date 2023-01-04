fd0a3ea053fb7b5155bfdbccf6f4a33aa287c025
Dyachkova <darya.i.dyachkova@gmail.com>
2023-01-04 18:52:54 +0300

    added a function "game"

--- main.py
+++ main.py
@@ -11,6 +11,19 @@ screen = pygame.display.set_mode(WSIZE)
 FPS = 100
 
 
+def game():
+    running = True
+    while running:
+        screen.fill((40, 40, 40))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+        clock.tick(FPS)
+
+
 def main_menu():
     running = True
     while running:
